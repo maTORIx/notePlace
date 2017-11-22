a4dafde3c85952e0145836f151857b220ace3985
maTORIx <matorixmatrix@gmail.com>
2017-11-22 15:12:53 +0900

    marked化成功

---
+++ app/javascript/packs/marked.js
@@ -0,0 +1,14 @@
+var marked = require('marked/marked.min.js')
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
+module.exports = marked
--- app/javascript/packs/tools.js
+++ app/javascript/packs/tools.js
@@ -1,34 +1,21 @@
-// const marked = require("marked/marked.min.js")
+var marked = require("./marked.js")
 
 module.exports = {
-  parseNoteDescription: function(src) {
+  parseHTML: function(src) {
     if(typeof src !== 'string') {
       return src;
     }
-    console.log("hello")
-    var text =  src.replace(/[&'`"<>]/g, function(match) {
-      return {
-        '&': '&amp;',
-        "'": '&#x27;',
-        '`': '&#x60;',
-        '"': '&quot;',
-        '<': '&lt;',
-        '>': '&gt;',
-      }[match]
-    });
 
-    text = text.replace(/(?:\s|^)#([^#\s]+)/mg, function(matched, tag) {
+    var text = src.replace(/(?:\s|^)#([^#\s]+)/mg, function(matched, tag) {
       let prefix = matched.slice(0, matched.length - tag.length - 1)
       return `${prefix}<a href='/search/notes/%23${tag}'>#${tag}</a>`
-    })
-
-    text = text.replace(/\n/g, function(tag) {
-      return `<br>`
     })
 
     text = text.replace(/(https?:\/\/[\w\-\.\/\?\,\#\:\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+)/g, function(url) {
       return `<a href="${url}">${url}</a>`
     })
+
+    text = marked(text)
 
     return text
   },
